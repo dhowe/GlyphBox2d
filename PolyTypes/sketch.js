f1b08ab3a10452da03bc715f669d9d523bc026c7
Daniel Howe <daniel@rednoise.org>
2015-08-03 10:27:34 -0400

    cleanup

--- PolyTypes/sketch.js
+++ PolyTypes/sketch.js
@@ -1,4 +1,4 @@
-var world, isStatic = false, font, txt = "p%5", x = 100, y = 150, fontSize = 150;
+var world, letter, isStatic = false, font, txts = ['%','p%','o%'], x = 100, y = 150, fontSize = 150;
 
 function preload() {
   font = loadFont("../fonts/AvenirNextLTPro-Demi.otf");
@@ -8,25 +8,30 @@ function setup() {
 
   createCanvas(500,700);
   background(100);
-  strokeWeight(.5);
+  strokeWeight(.3);
   //noStroke();
 
   var noHoles = false;
-  var libs = [ 'decompose', 'triangulatePoly2Tri', 'triangulateEarcut', 'triangulatePnltri' ];
+  //var libs = [ 'tessDecomp', 'tessPoly2Tri', 'tessEarcut', 'tessPnltri' ];
+  var libs = [ 'tessPoly2Tri', 'tessEarcut', 'tessPnltri' ];
+  //libs = [ 'tessEarcut', 'tessEarcut','tessEarcut'];
   for (var h = 0; h < libs.length; h++) {
-
-    var tris = getPolygonTris(libs[h], noHoles), yoff = h * 150, total = 0;
-    console.log(tris.length+" glyph");
-    for (var k = 0; k < tris.length; k++) {
-      var ptlist = tris[k];//k * fontSize*.7;
+    var txt = 'p%';//txts[h];
+    var tris = getPolygonTris(libs[h], txt, noHoles), yoff = h * 160, total = 0;
+    for (var k = 0; k < tris.length; k++)
+    {
+      var ptlist = tris[k];
       push();
+      stroke(0);
+      noFill();
       translate(0,yoff);
-      console.log(ptlist.length+" polys", ptlist);
+      //console.log(ptlist.length+" polys", ptlist);
       for (var i = 0; i < ptlist.length; i++) {
         fill(random(0,255),random(0,255),random(0,255));
         beginShape();
         for (var j = 0; j < ptlist[i].length; j++) {
           vertex(ptlist[i][j].x, ptlist[i][j].y);
+          //ellipse(ptlist[i][j].x, ptlist[i][j].y,3,3);
           //console.log(ptlist[j].x, ptlist[j].y);
         }
         endShape(CLOSE);
@@ -38,11 +43,11 @@ function setup() {
     line(0, y+yoff,width, y+yoff);
     fill(255);
     noStroke();
-    text(libs[h].replace('triangulate',''), x-50, y+yoff);
+    text(libs[h].replace('tess',''), x-50, y+yoff);
   }
 }
 
-function getPolygonTris(trilib, noHoles) { // returns set of 2d[] of tris
+function getPolygonTris(trilib, txt, noHoles) { // returns set of 2d[] of tris
 
   var result = [], glyphs = font._getGlyphs(txt), xoff = 0;
 
@@ -50,10 +55,32 @@ function getPolygonTris(trilib, noHoles) { // returns set of 2d[] of tris
 
     var polys = getPolys(glyphs[i], x + xoff, y, fontSize);
 
+    console.log(glyphs[i].name+': '+polys.length+' poly(s)');
+    letter = glyphs[i].name;
+//if (i==0) continue;
+
     // then get polygons and pts
     for (var j = 0; j < polys.length; j++) {
 
       polys[j].simplify();
+
+    //  if (i>0&&j!==2)
+        console.log('  #'+(j+1)+') '+polys[j].points.length+' points, '+
+          (polys[j].holes ? polys[j].holes.length : 0) +
+          ' hole(s)');//polys[j].points.length);
+
+      if (polys[j].holes) {
+        var s = '';
+        for (var k = 0; k < polys[j].holes[0].length; k++) {
+          if (i>0&&j==1) {
+            var px = polys[j].holes[0][k].x+94.95000000000004;
+            var py = polys[j].holes[0][k].y;
+            s += px+', '+py;
+          }
+        }
+        //console.log(s);
+      }
+
       polys[j][trilib](noHoles);
       result.push(polys[j].triangles);
     }
