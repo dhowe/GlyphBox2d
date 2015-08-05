ee93e6d425aa8f6a78508f5322f2f45f49975412
Daniel Howe <daniel@rednoise.org>
2015-08-05 07:00:28 -0400

    start on api

--- GlyphFlock/sketch.js
+++ GlyphFlock/sketch.js
@@ -9,7 +9,10 @@ function setup() {
   createCanvas(500,300);
   flock = new Flock();
   loadFont("avenir.otf", function(f) {
-      createFlock(f, 80, 185, 'p5.js', 150);
+      var points = pointsFromText(f, 80, 185, 'p5.js', 150);
+      for (var k = 0; k < points.length; k++) {
+        flock.boids.push(new Boid(points[k]));
+      }
   });
 }
 
@@ -18,27 +21,6 @@ function draw() {
   var c = flock.count / flock.boids.length;
   background(c * 237, 34, 93);
   flock.run();
-}
-
-function createFlock(font, x, y, txt, size) {
-
-  var xoff = 0, glyphs = font._getGlyphs(txt);
-
-  for (var i = 0; i < glyphs.length; i++) { // each glyph
-
-    // sample every 10th of glyph path length
-    var polys = getPolys(glyphs[i], x, y, size, { sampleFactor: .1 });
-    for (var j = 0; j < polys.length; j++) {
-
-      // TODO: getPoints(glyph, size, options); ??
-      var points = polys[j].getPoints();
-      for (var k = 0; k < points.length; k++) {
-        points[k].x += xoff;
-        flock.boids.push(new Boid(points[k]));
-      }
-    }
-    xoff += glyphs[i].advanceWidth * font._scale(size);
-  }
 }
 
 function mouseReleased() {
